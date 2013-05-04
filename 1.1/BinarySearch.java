0d384f7e58fe8fd21c5336f181d5c87e5008aae8
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-05-04 22:25:17 +0300

    binary search page 9

---
+++ 1.1/BinarySearch.java
@@ -0,0 +1,40 @@
+//page 9
+//run with C:\java\Sedgewick\1.1>javac -classpath c:\java\Sedgewick\1.1\stdlib.jar  BinarySearch.java -Xlint:deprecation
+
+import java.util.Arrays;
+
+public class BinarySearch
+{
+	public static int rank(int key, int[] a)
+	{
+		int left=0;
+		int right=a.length-1;
+		while(left<=right)
+		{
+			int middle=(left+right)/2;
+			if(a[middle]<key)
+				left=middle+1;
+			else
+				if(a[middle]>key)
+					right=middle-1;
+				else
+					return middle;
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] whitelist=In.readInts(args[0]);
+		
+		Arrays.sort(whitelist);
+		
+		while(!StdIn.isEmpty())
+		{
+			int key=StdIn.readInt();
+			if(rank(key,whitelist)==-1)
+				StdOut.println(key);
+		}
+	}
+}
