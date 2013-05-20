10b473dbf55e2c4a52e8d69b5a175b2297751a27
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-05-20 07:51:44 +0300

    stack with circular linked

---
+++ 1.3/CircularLinkedListIterator.java
@@ -0,0 +1,36 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CircularLinkedListIterator<Item extends Comparable<Item>> implements Iterator<Item>
+{
+	private CircularLinkedList<Item>.Node currentNode;
+	private CircularLinkedList<Item>.Node lastNode;
+
+	public CircularLinkedListIterator(CircularLinkedList<Item> list)
+	{
+		lastNode=list.getFirst();
+		if(lastNode!=null)
+			currentNode=lastNode.next;
+		else
+			currentNode=null;
+	}
+
+	public boolean hasNext()
+	{
+		return currentNode!=null||currentNode!=lastNode;
+	}
+	
+	public Item next()
+	{
+		if(currentNode==null||currentNode==lastNode)
+			throw new NoSuchElementException();
+		Item returnValue=currentNode.item;
+		currentNode=currentNode.next;
+		return returnValue;
+	}
+	
+	public void remove()
+	{
+		throw new UnsupportedOperationException();
+	}
+} 
