85e1d77f8864251f93ab216d03fa2e5edd3343b5
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-06-03 00:58:56 +0300

    local minimum in array

---
+++ 1.4/LocalMinInArray.java
@@ -0,0 +1,51 @@
+//1.4.18
+//page 210
+
+public class LocalMinInArray
+{
+	public Result find(int[] a)
+	{
+		return find(a,0,a.length-1);
+	}
+	
+	private Result find(int[] a, int left, int right)
+	{
+		while(right-left>=2&&left>=0&&right<=a.length-1)
+		{
+			int middle=(right+left+1)/2;
+			if(a[middle-1]>a[middle]&&a[middle+1]>a[middle])
+				return new Result(middle);
+			else
+			{
+				Result result=null;
+				if(a[middle-1]<a[middle+1])
+					result=find(a,left,middle);
+				if(result==null)
+					return find(a,middle+1,right);
+				else return result;
+			}
+		}
+		
+		return null;
+	}
+	
+	public class Result
+	{
+		public int position;
+		public Result(int pos)
+		{
+			position=pos;
+		}
+	}
+	
+	public static void main(String[] args)
+	{	
+		LocalMinInArray l=new LocalMinInArray();
+		int[] a={-5,-1,1,9,10,7,11};
+		Result res=l.find(a);
+		if(res==null)
+			StdOut.println("none found");
+		else
+			StdOut.println(a[res.position]);
+	}
+}
