01de841a69dfa1df844b672d02c11eebcd0534ac
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-06-18 09:50:36 +0300

    insertion sort with sentinel

--- 2.1/InsertionSortWithSentinel.java
+++ 2.1/InsertionSortWithSentinel.java
@@ -1,4 +1,36 @@
 //2.1.24
 //page 267
 
+<<<<<<< HEAD
 public class InsertionSortWithSentinel
+=======
+public class InsertionSortWithSentinel
+{
+	public static void sort(Comparable[] a)
+	{
+		for(int i=1;i<a.length;i++)
+		{
+			if(a[i].compareTo(a[0])<0)
+			{
+				Comparable item=a[i];
+				a[i]=a[0];
+				a[0]=item;
+			}
+			for(int j=i;a[j].compareTo(a[j-1])<0;j--)
+			{
+				Comparable item=a[j];
+				a[j]=a[j-1];
+				a[j-1]=item;
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		String[] a=In.readStrings();
+		sort(a);
+		for(int i=0;i<a.length;i++)
+			StdOut.print(a[i]+" ");
+	}
+}
+>>>>>>> insertion sort with sentinel
---
+++ 2.2/TopDownMergeSort.java
@@ -0,0 +1,11 @@
+//algorithm 2.4
+//page 271+273
+
+public class TopDownMergeSort
+{
+	public static void sort()
+	{
+	}
+	
+	public static void merge
+}
