49ff8f19f47c4a799d10493435f05ca5644f50d4
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-09-27 12:57:08 +0300

    perfect balance

--- 3.2/PerfectBalance.java
+++ 3.2/PerfectBalance.java
@@ -1,8 +1,24 @@
+//3.2.25 (page 419)
+
+import java.util.Arrays;
+
 public class PerfectBalance
 {
+	private static void balance(BST<String,Integer> bst,String[] input,int lo,int hi)
+	{
+		if(hi<lo) return;
+		int mid=lo+(hi-lo)/2;
+		bst.put(input[mid],mid);
+		StdOut.println(input[mid]+" "+mid);
+		balance(bst,input,lo,mid-1);
+		balance(bst,input,mid+1,hi);
+	}
+	
 	public static void main(String[] args)
 	{
 		String[] input=StdIn.readAll().split("\\s+");
 		Arrays.sort(input);
+		BST<String,Integer> bst=new BST<String,Integer>();
+		balance(bst,input,0,input.length-1);
 	}
 }
