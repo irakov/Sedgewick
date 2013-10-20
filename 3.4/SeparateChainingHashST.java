1726e0fc22da3fc57270b2092538e8df2a38e991
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-10-20 10:18:03 +0300

    separate chaining hash with array resizing

--- 3.4/SeparateChainingHashST.java
+++ 3.4/SeparateChainingHashST.java
@@ -1,28 +1,40 @@
 //algorithm 3.5(465)
 //3.4.19(482)
+//with resizing
 
 public class SeparateChainingHashST<Key extends Comparable<Key>,Value>
 {
-	private int base;
-	private int size;
+	private int keyCount;
+	private int hashSize;
 	private SequentialSearchST<Key,Value>[] st;
 	
 	public SeparateChainingHashST()
 	{
-		this(997);//some prime number, base for hashing
+		this(4);//some prime number, base for hashing
 	}
 	
-	public SeparateChainingHashST(int base)
+	public SeparateChainingHashST(int hashSize)
 	{
-		this.base=base;
-		st=(SequentialSearchST<Key,Value>[])new SequentialSearchST[base];
+		this.hashSize=hashSize;
+		st=(SequentialSearchST<Key,Value>[])new SequentialSearchST[hashSize];
 		for(int i=0;i<st.length;i++)
 			st[i]=new SequentialSearchST();
 	}
 	
+	private void resize(int newSize)
+	{
+		SeparateChainingHashST<Key,Value> temp=new SeparateChainingHashST<Key,Value>(newSize);
+		for(int i=0;i<hashSize;i++)
+			for(Key key:st[i].keys())
+				temp.put(key,st[i].get(key));
+		keyCount=temp.keyCount;
+		hashSize=newSize;
+		st=temp.st;
+	}
+	
 	private int hash(Key key)
 	{
-		return (key.hashCode()&0x7fffffff)%base;
+		return (key.hashCode()&0x7fffffff)%hashSize;
 	}
 	
 	public Value get(Key key)
@@ -32,12 +44,21 @@ public class SeparateChainingHashST<Key extends Comparable<Key>,Value>
 	
 	public void put(Key key,Value value)
 	{
+		if(value==null) delete(key);
+		
+		if(keyCount>=10*hashSize) resize(2*hashSize);
+		
+		if(!contains(key)) keyCount++;
 		st[hash(key)].put(key,value);
 	}
 	
 	public void delete(Key key)
 	{
+		if(!contains(key)) return;
 		st[hash(key)].delete(key);
+		keyCount--;
+		
+		if(keyCount<=2*hashSize) resize(hashSize/2);
 	}
 	
 	public Iterable<Key> keys()
@@ -48,6 +69,12 @@ public class SeparateChainingHashST<Key extends Comparable<Key>,Value>
 				queue.enqueue(key);
 		return queue;
 	}
+	
+	public int size(){return keyCount;}
+	
+	public boolean isEmpty(){return size()==0;}
+	
+	public boolean contains(Key key){return get(key)!=null;}
 	
 	public static void main(String[] args)
 	{
