cf221cba4324fa70ba4ea220a0e4f667f74ff7c7
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-10-20 22:42:11 +0300

    linear probing hash st wip

--- 3.4/LinearProbingHashST.java
+++ 3.4/LinearProbingHashST.java
@@ -4,13 +4,24 @@
 public class LinearProbingHashST<Key extends Comparable<Key>,Value>
 {
 	private int keyCount;
-	private int tableSize;
+	private int capacity;
+	private int logCapacity;
 	private Key[] keys;
 	private Value[] values;
 	
+	private static final int INITIAL_LOG_CAPACITY=3;
+	private static final int INITIAL_CAPACITY=8;
+	private static final int[] PRIMES={
+		//3,4,5,6,7,8,9,10
+		//8,16,32,64,128,256,512,1024... logCapacity-3
+		7, 13, 19, 31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381,
+		32749, 65521, 131071, 262139, 524287, 1048573, 2097143, 4194301,
+		8388593, 16777213, 33554393, 67108859, 134217689, 268435399,
+		536870909, 1073741789, 2147483647};
+	
 	public LinearProbingHashST()
 	{
-		this(16);
+		this(INITIAL_CAPACITY,INITIAL_LOG_CAPACITY);
 	}
 	
 	public LinearProbingHashST(int tableSize)
