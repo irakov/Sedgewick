b2042affe25eda99f06f01f0cd3c917bf35c5ddf
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-10-24 18:29:15 +0300

    hash set wip

---
+++ 3.5/HashSET.java
@@ -0,0 +1,39 @@
+//3.5.1 (507)
+
+public class HashSET<Key extends Comparable<Key>>
+{
+	private LinearProbingHashST<Key,Integer> hash;
+	
+	public HashSET()
+	{
+		hash=new LinearProbingHashST<Key,Integer>();
+	}
+	
+	public void add(Key key)
+	{
+	}
+	
+	public void delete(Key key)
+	{
+	}
+	
+	public boolean contains(Key key)
+	{
+	}
+	
+	public boolean isEmpty()
+	{
+	}
+	
+	public int size()
+	{
+	}
+	
+	public String toString()
+	{
+	}
+	
+	public static void main(String[] args)
+	{
+	}
+}
