61a7b43709b939bfe1b7ec0008787d8451ffa6b7
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-10-24 09:14:51 +0300

    set api

---
+++ 3.5/SET.java
@@ -0,0 +1,41 @@
+//3.5.1 (507)
+
+public class SET<Key extends Comparable<Key>>
+{
+	private RedBlackBST<Key,Integer> bst;
+	
+	public SET()
+	{
+		bst=new RedBlackBST<Key,Integer>();
+	}
+	
+	public void add(Key key)
+	{
+		//todo
+	}
+	
+	public void delete(Key key)
+	{
+		//todo
+	}
+	
+	public boolean contains(Key key)
+	{
+		//todo
+	}
+	
+	public boolean isEmpty()
+	{
+		//todo
+	}
+	
+	public int size()
+	{
+		//todo
+	}
+	
+	public String toString()
+	{
+		//todo
+	}
+}
