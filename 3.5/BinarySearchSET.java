f3d6601ebe6fb1930ed3a24d0d9f25ded95014c0
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-11-06 13:22:15 +0200

    more on BinarySearchSET

--- 3.5/BinarySearchSET.java
+++ 3.5/BinarySearchSET.java
@@ -1,3 +1,5 @@
+//3.5.3 (page 507)
+
 import java.util.Scanner;
 import java.io.BufferedInputStream;
 import java.io.PrintWritter;
@@ -24,7 +26,8 @@ public class BinarySearchSET<Key extends Comparable<Key>>
 		else
 		{
 			int rank=rank(key);
-			if(rank>=size||keys[rank].compareTo(key)!=0)
+			if(rank<size&&keys[rank].compareTo(key)==0);
+			else
 			{
 				if(keys.length==size) resize(2*size);
 				for(int i=size;i>rank;i--)
@@ -37,6 +40,73 @@ public class BinarySearchSET<Key extends Comparable<Key>>
 
 	public Key get(Key key)
 	{
-	
+		if(isEmpty()) return;
+
+		int rank=rank(key);
+		if(rank<size&&keys[rank].compareTo(key)==0) return key;
+		return;
+	}
+
+	public void delete(Key key)
+	{
+		if(isEmpty()) return;
+
+		int rank=rank(key);
+		if(rank<size&&keys[rank].compareTo(key)==0)
+		{
+			for(int i=rank;i<size-1;i++)
+				keys[i]=keys[i+1];
+			keys[size-1]=null;
+			size--;
+			if(keys.length<=size/4) resize(size/2);
+		}
+	}
+
+	public boolean contains(Key key)
+	{
+		return get(key)!=null;
+	}
+
+	public boolean isEmpty()
+	{
+		return size==0;
+	}
+
+	public int size()
+	{
+		return size;
+	}
+
+	public Key min()
+	{
+		if(isEmpty()) return null;
+		return keys[0];
+	}
+
+	public Key max()
+	{
+		if(isEmpty()) return null;
+		return keys[size-1];
+	}
+
+	public Key floor(Key key)
+	{
+		if(isEmpty()) return null;
+
+		int rank=rank(key);
+		if(rank<size&&keys[rank].compareTo(key)==0)
+			if(rank==0) return null;
+			else return keys[rank-1];
+		else return null;
+	}
+
+	public Key ceiling(Key key)
+	{
+		if(isEmpty()) return null;
+
+		int rank=rank(key);
+		if(rank<size&&keys[rank].compareTo(key)==0)
+			return keys[rank];
+		return null;
 	}
 }
