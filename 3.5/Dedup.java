223b9fe40c791ee0509892d9166b3fedf1f5bee2
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-11-08 09:38:03 +0200

    dedup

---
+++ 3.5/Dedup.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+
+public class Dedup
+{
+	public static void main(String[] args)
+	{
+		Scanner input=new Scanner(new BufferedInputStream(System.in));
+		PrintWriter output=new PrintWriter(new OutputStreamWriter(System.out),true);
+	
+		HashSET<String> hash=new HashSET<String>();
+		while(input.hasNext())
+		{
+			String s=input.next();
+			if(!hash.contains(s))
+			{
+				hash.add(s);
+				output.println(s);
+			}
+		}
+	}
+}
