bdfc539e6b4c5470435fde9d5d4b1b7d44727226
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2013-11-09 18:53:46 +0200

    lookupcsv

--- 3.5/LookupCSV.java
+++ 3.5/LookupCSV.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+
 public class LookupCSV
 {
 	public static void main(String[] args)
@@ -16,13 +23,24 @@ public class LookupCSV
 		}
 		catch(IOException ex)
 		{
-			System.err.println(ex);
+		System.err.println(ex);
 		}
 		
 		RedBlackBST<String,String> st=new RedBlackBST<String,String>();
 		while(fileInput.hasNextLine())
 		{
 			String line=fileInput.nextLine();
+			String[] items=line.split(",");
+			st.put(items[keyField],items[valueField]);
+		}
+		
+		Scanner input=new Scanner(new BufferedInputStream(System.in));
+		PrintWriter output=new PrintWriter(new OutputStreamWriter(System.out),true);
+		while(input.hasNext())
+		{
+			String s=input.next();
+			if(st.contains(s))
+				output.println(st.get(s));
 		}
 	}
 }
