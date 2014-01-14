d27d2fd1ca0aa08d8ffa4b7b299929db7cf9372b
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2014-01-14 17:21:21 +0200

    symbol digraph

--- 4.2/Digraph.java
+++ 4.2/Digraph.java
@@ -17,7 +17,7 @@ public class Digraph
 	private void buildGraph(int V)
 	{
 		if(V<0) throw new IllegalArgumentException();
-		vertices=v;
+		vertices=V;
 		edges=0;
 		adjency=(Bag<Integer>[])new Bag[vertices];
 		for(int i=0;i<vertices;i++) adjency[i]=new Bag<Integer>();
@@ -44,7 +44,7 @@ public class Digraph
 		int E=fileInput.nextInt();
 		if(E<0) throw new IllegalArgumentException();
 		edges=E;
-		for(int i=0;i<edges;i++)
+		for(int i=0;i<E;i++)
 		{
 			int v=fileInput.nextInt();
 			int w=fileInput.nextInt();
@@ -64,27 +64,46 @@ public class Digraph
 	
 	public void addEdge(int v,int w)
 	{
-		...
-	}
-	
-	public Iterable<Integer> adj(int v)
-	{
 		if(v<0||v>=vertices) throw new IndexOutOfBoundsException();
 		if(w<0||w>=vertices) throw new IndexOutOfBoundsException();
 		edges++;
 		adjency[v].add(w);
 	}
 	
+	public Iterable<Integer> adj(int v)
+	{
+		if(v<0||v>=vertices) throw new IndexOutOfBoundsException();
+		return adjency[v];
+	}
+	
 	public Digraph reverse()
 	{
-		Graph reverse=
+		Digraph reverse=new Digraph(vertices);
+		for(int i=0;i<vertices;i++)
+			for(int j:adj(i))
+				reverse.addEdge(j,i);
+		return reverse;
 	}
 	
 	public String toString()
 	{
+		StringBuilder sb=new StringBuilder();
+		sb.append(vertices+" vertices "+edges+" edges"+System.getProperty("line.separator"));
+		for(int i=0;i<vertices;i++)
+		{
+			sb.append(i+": ");
+			for(int j:adj(i)) sb.append(j+" ");
+			sb.append(System.getProperty("line.separator"));
+		}
+		return sb.toString();
 	}
 	
 	public static void main(String[] args)
 	{
+		Digraph g=new Digraph("test.txt");
+		PrintWriter output=new PrintWriter(new OutputStreamWriter(System.out),true);
+		output.println(g);
+		output.println("<<<reverse>>>");
+		output.println(g.reverse());
 	}
 }
