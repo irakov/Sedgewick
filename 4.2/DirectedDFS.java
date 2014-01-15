953d0791995dfc4527c7d4e8b39647854b7791b2
Bogdan Ungureanu <bogdan_petru_ungureanu@yahoo.com>
2014-01-15 02:34:48 +0200

    directed dfs done

--- 4.2/DirectedDFS.java
+++ 4.2/DirectedDFS.java
@@ -1,5 +1,8 @@
 //page 570 (algorithm 4.4)
 
+import java.io.*;
+import java.util.*;
+
 public class DirectedDFS
 {	
 	private boolean[] marked;
@@ -7,7 +10,7 @@ public class DirectedDFS
 	public DirectedDFS(Digraph g,int s)
 	{
 		marked=new boolean[g.V()];
-		dfs(G,s);
+		dfs(g,s);
 	}
 	
 	public DirectedDFS(Digraph g,Iterable<Integer> sources)
@@ -15,14 +18,32 @@ public class DirectedDFS
 		marked=new boolean[g.V()];
 		for(int s:sources)
 			if(!marked[s])
-				dfs(G,s);
+				dfs(g,s);
+	}
+	
+	private void dfs(Digraph g,int s)
+	{
+		marked[s]=true;
+		for(int i:g.adj(s))
+			if(!marked[i])
+				dfs(g,i);
 	}
 	
 	public boolean marked(int v)
 	{
+		return marked[v];
 	}
 	
 	public static void main(String[] args)
 	{
+		PrintWriter output=new PrintWriter(new OutputStreamWriter(System.out),true);
+	
+		String fileName=args[0];
+		int source=Integer.parseInt(args[1]);
+		Digraph g=new Digraph(fileName);
+		DirectedDFS dfs=new DirectedDFS(g,source);
+		
+		for(int i=0;i<g.V();i++)
+			output.println("is "+i+" reachable from "+source+":"+dfs.marked(i));
 	}
 }
