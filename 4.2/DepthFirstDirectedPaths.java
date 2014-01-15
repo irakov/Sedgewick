41b6325d5a40d6988d76891f730d97484855d894
Bogdan Ungureanu <bogdan_petru_ungureanu@yahoo.com>
2014-01-15 03:17:30 +0200

    depth first directed paths wip

---
+++ 4.2/DepthFirstDirectedPaths.java
@@ -0,0 +1,23 @@
+//page 573
+
+public class DepthFirstDirectedPaths
+{
+	public DepthFirstDirectedPaths(Digraph g,int s)
+	{
+	...
+	}
+	
+	public boolean hasPathTo(int v)
+	{
+	...
+	}
+	
+	public Iterable<Integer> pathTo(int v)
+	{
+	...
+	}
+	
+	public static void main(String[] args)
+	{
+	}
+}
