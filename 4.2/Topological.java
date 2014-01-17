962c3e25892c6ca1edb9c2def43f1194e597a2fe
DaeMoohn <bogdan_petru_ungureanu@yahoo.com>
2014-01-17 19:22:31 +0200

    topological sort

---
+++ 4.2/Topological.java
@@ -0,0 +1,42 @@
+//page 578+581(algorithm 4.5)
+
+import java.io.*;
+
+public class Topological
+{
+	private Iterable<Integer> order;
+	
+	public Topological(Digraph g)
+	{
+		DirectedCycle dc=new DirectedCycle(g);
+		if(!dc.hasCycle())
+		{
+			DepthFirstOrder dfo=new DepthFirstOrder(g);
+			order=dfo.reversePostOrder();
+		}
+	}
+	
+	public boolean isDAG()
+	{
+		return order!=null;
+	}
+	
+	public Iterable<Integer> order()
+	{
+		return order;
+	}
+	
+	public static void main(String[] args)
+	{
+		PrintWriter output=new PrintWriter(new OutputStreamWriter(System.out),true);
+		
+		String fileName=args[0];
+		String delim=args[1];
+		SymbolDigraph sd=new SymbolDigraph(fileName,delim);
+		Topological t=new Topological(sd.G());
+		
+		for(int i:t.order())
+			output.println(sd.name(i));
+		
+	}
+}
